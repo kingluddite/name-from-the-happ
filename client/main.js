042885f15920a106e80def4be2489951629f2aa3
Philip Howley <howley.phil@gmail.com>
2017-05-24 21:54:01 -0700

    add sessions suggested routes and scalable router auth

--- client/main.js
+++ client/main.js
@@ -1,13 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+import { browserHistory } from 'react-router';
 import onAuthChange from './../imports/startup/client/routes';
 
 import './../imports/startup/client';
 // collections
 import './../imports/api/sections';
 
-
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  onAuthChange(isAuthenticated);
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+
+  // console.log('currentPagePrivacy', currentPagePrivacy);
+  onAuthChange(isAuthenticated, currentPagePrivacy);
+});
+
+Tracker.autorun(() => {
+  const selectedSectionId = Session.get('selectedSectionId');
+
+  if (selectedSectionId) {
+    browserHistory.replace(`/sections/${selectedSectionId}`);
+  }
+});
+
+Tracker.autorun(() => {
+  const selectedPresentationId = Session.get('selectedPresentationId');
+
+  if (selectedPresentationId) {
+    browserHistory.replace(`/presentations/${selectedPresentationId}`);
+  }
 });
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,37 +1,43 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Session } from 'meteor/session';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
 // components
 import App from './../../ui/components/App';
 import Login from './../../ui/components/Login';
 import Signup from './../../ui/components/Signup';
 import Sections from './../../ui/components/Sections';
-import PresentationList from './../../ui/components/PresentationList';
-import PresentationView from './../../ui/components/PresentationView';
-import AddPresentation from './../../ui/components/AddPresentation';
-import PresentationEdit from './../../ui/components/PresentationEdit';
+// import NewSection from './../../ui/components/NewSection';
+// import EditSection from './../../ui/components/EditSection';
+// import ViewSection from './../../ui/components/ViewSection';
+// import Presentations from './../../ui/components/Presentations';
+// import NewPresentation from './../../ui/components/NewPresentation';
+// import EditPresentation from './../../ui/components/EditPresentation';
+// import ViewPresentation from './../../ui/components/ViewPresenation';
 import NotFound from './../../ui/components/NotFound';
 
-const publicPages = ['/', '/signup'];
-const privatePages = ['/sections'];
+// const onEnterPresentationPage = (nextState) => {
+//   Session.set('selectedPresentationId', nextState.params.id);
+// };
+//
+// const onLeavePresentationPage = () => {
+//   Session.set('selectedPresentationId', undefined);
+// };
+//
+// const onEnterSectionPage = (nextState) => {
+//   Session.set('selectedSectionId', nextState.params.id);
+// };
+//
+// const onLeaveSectionPage = () => {
+//   Session.set('selectedSectionId', undefined);
+// };
 
-const onEnterPublicPage = () => {
-  if (Meteor.userId()) {
-    browserHistory.replace('/sections');
-  }
-};
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
-
-const onAuthChange = (isAuthenticated) => {
-  const pathname = browserHistory.getCurrentLocation().pathname;
-  const isPublicPage = publicPages.includes(pathname);
-  const isPrivatePage = privatePages.includes(pathname);
+const onAuthChange = (isAuthenticated, currentPagePrivacy) => {
+  const isPublicPage = currentPagePrivacy === 'unauth';
+  const isPrivatePage = currentPagePrivacy === 'auth';
 
   // if public page and logged in - let them in
   if (isPublicPage && isAuthenticated) {
@@ -42,34 +48,35 @@ const onAuthChange = (isAuthenticated) => {
   }
 };
 
+export const globalOnEnter = (nextState) => {
+  const lastRoute = nextState.routes[nextState.routes.length - 1];
+  Session.set('currentPagePrivacy', lastRoute.privacy);
+};
+
+export const globalOnChange = (prevState, nextState) => {
+  globalOnEnter(nextState);
+};
+
 Meteor.startup(() => {
+  Session.set('selectedSectionId', undefined);
+  Session.set('selectedPresentationId', undefined);
   render(
     <Router history={ browserHistory }>
-      <Route path="/" component={ App }>
-        <IndexRoute component={ Login } onEnter={ onEnterPublicPage }/>
-        <Route path="/signup" component={ Signup} onEnter={ onEnterPublicPage } />
-        <Route path="/sections" component={ Sections } onEnter={onEnterPrivatePage} />
-        <Route
-          path="/presentationlist"
-          component={ PresentationList }
-        />
-        <Route
-          path="/presentationview"
-          component={ PresentationView }
-          onEnter={onEnterPrivatePage}
-        />
-        <Route
-          path="/presentationadd"
-          component={ AddPresentation }
-          onEnter={onEnterPrivatePage}
-        />
-        <Route
-          path="/presentationedit"
-          component={PresentationEdit}
-          onEnter={onEnterPrivatePage}
-        />
-        <Route path="*" component={ NotFound } />
+      <Route onEnter={globalOnEnter} onChange={globalOnChange}>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ Login } privacy="unauth"/>
+          <Route path="/signup" component={ Signup} privacy="unauth" />
+          <Route path="/sections" component={ Sections } privacy="auth" />
+          {/* <Route name="newSection" path="/sections/new" component={ NewSection } privacy="auth" /> */}
+          {/* <Route name="editSection" path="/sections/:_id/edit" component={ EditSection } privacy="auth" /> */}
+          {/* <Route name="viewSection" path="/sections/:_id" component={ ViewSection } privacy="auth" onEnter={onEnterSectionPage} onLeave={onLeaveSectionPage} /> */}
+          {/* <Route path="/presentations" component={ Presentations } privacy="auth" /> */}
+          {/* <Route name="newPresentation" path="/presentations/new" component={ NewPresentation } privacy="auth" /> */}
+          {/* <Route name="editPresentation" path="/presentations/:_id/edit" component={ EditPresentation } privacy="auth" /> */}
+          {/* <Route name="viewPresentation" path="/presentations/:_id" component={ ViewPresentation } onEnter={onEnterPresentationPage} onLeave={onLeavePresentationPage} privacy="auth" /> */}
+          <Route path="*" component={ NotFound } />
       </Route>
+    </Route>
     </Router>,
     document.getElementById('react-root'),
   );
